13308f9e829e78756a75a7105195e798af26739d
Sam Goldie <samuelmgoldie@gmail.com>
2020-12-28 12:14:40 -0800

    Here's a bunch of random, mostly non-stable stuff

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+console.log('hello folks!');
+
+// function App() {
+//   return (
+//     <div>
+//       <input type='text' onChange={() => handleInput(this.event.target)}>
+//     </input>
+//     <div>
+//       HELLOOOOOOOOO
+//     </div>
+//     <div>
+//     <iframe width="560" height="315" src="https://www.youtube.com/embed/IY9YNF5MMQo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+//     </div>
+//     </div>
+//   )
+// }
+
+function App() {
+  return (
+    <div>
+      HELLO
+    </div>
+  )
+}
+
+export default App;
