6f2d23798cc27fb4cef312317952baff8b7d978d
Sam Goldie <samuelmgoldie@gmail.com>
2020-12-28 09:51:16 -0800

    Server running locally. Basic HTML available

---
+++ public/app.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+console.log('hello folks!');
+
+const App = () => {
+  const [input, handleInput] = useState('');
+
+  return (
+    <div>
+      <input type='text' onChange={() => handleInput(this.event.target)}>
+    </div>
+    <div>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/IY9YNF5MMQo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+  )
+}
