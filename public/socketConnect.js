de3d029dad5d386c7ed5b3502ec8af199492778b
Sam Goldie <samuelmgoldie@gmail.com>
2021-01-04 12:23:58 -0800

    Grid fixed, chatbar dimensions good, sockets firing on chat messages

--- public/socketConnect.js
+++ public/socketConnect.js
@@ -1,10 +1,19 @@
 // turns out, iframes keep track of time data more granularly than seconds. implement greater precision of playback sync when you have time
 
+
+// const newMessage = document.createElement('div');
+// newMessage.append(`Me  ${addedMessage.body}`);
+// const chatContainer = document.getElementById('chat-container');
+// chatContainer.append(newMessage);
+// chatContainer.scrollBy(0, chatContainer.scrollHeight);
+
 socket.on('message', (data) => {
   console.log(`data is: ${data}`);
   const newMessage = document.createElement('div');
   newMessage.append(`${data.user}  ${data.body}`);
-  document.getElementById('chat-container').append(newMessage);
+  const chatContainer = document.getElementById('chat-container');
+  chatContainer.append(newMessage);
+  chatContainer.scrollBy(0, chatContainer.scrollHeight);
 });
 
 socket.on('play', (timecode) => {
