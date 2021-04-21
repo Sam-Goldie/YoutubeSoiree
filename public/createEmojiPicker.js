bfb15ecebfca31ebbf3a61a4c354d1d600ad4738
Sam Goldie <samuelmgoldie@gmail.com>
2021-04-21 13:42:15 -0400

    Successfully transmitted nonces

---
+++ public/createEmojiPicker.js
@@ -0,0 +1,3 @@
+const emojiPicker = document.createElement('emoji-picker');
+
+document.getElementById('chat-container').append(emojiPicker);
--- server/index.js
+++ server/index.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const escapeInput = require('./escapeInput.js');
+const fs = require('fs');
 
 const port = 3000;
 
 const app = express();
+
+const nonceMaker = () => {
+  const validChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let nonce = '';
+  for (let i = 0; i < 32; i++) {
+    nonce += validChars[Math.floor(Math.random() * validChars.length)];
+  }
+  return nonce;
+}
+
+const newNonce = nonceMaker();
+
+let htmlContent = fs.readFileSync('indexTemplate.html', 'utf-8');
+console.log(htmlContent);
+htmlContent = htmlContent.replace('<emoji-picker></emoji-picker>',
+`<script nonce=${newNonce}>
+    import { Picker } from 'emoji-picker-element';
+    const picker = new Picker();
+    document.body.appendChild(picker);
+</script>`
+);
+htmlContent = htmlContent.replace(`default-src 'self'`, `default-src 'self' nonce-${newNonce}`);
+console.log(htmlContent);
+fs.writeFileSync('./public/index.html', htmlContent, 'utf-8');
 
 app.use(express.static('public'));
 
